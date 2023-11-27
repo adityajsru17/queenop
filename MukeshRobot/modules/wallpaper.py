b55288b250f386da57f01c23ce5104e212190a47
adityajsru17 <148866502+adityajsru17@users.noreply.github.com>
2023-11-27 16:44:03 +0530

    Update wallpaper.py

--- MukeshRobot/modules/wallpaper.py
+++ MukeshRobot/modules/wallpaper.py
@@ -52,7 +52,7 @@ async def wall(client, message):
 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
 ❄ **ᴅᴏᴡɴʟᴏᴀᴅ :** `{murl}`
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+╰☆☆ : ̗̀➛ Bყ ➳ [Aɱαɳ](https://t.me/hoi_itz_mee) •.¸♡ ☆☆╮
 """
         await m.delete()
         await client.send_photo(
