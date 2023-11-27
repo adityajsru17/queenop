5cf5038b61054ffd0b2f3bb54bec322190a1d02f
adityajsru17 <148866502+adityajsru17@users.noreply.github.com>
2023-11-27 19:26:37 +0530

    Update notes.py

--- MukeshRobot/modules/notes.py
+++ MukeshRobot/modules/notes.py
@@ -516,7 +516,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
  ❍ `/removeallnotes`*:* removes all notes from the group
  *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+╰☆☆《....《☆ 𝕭ყ ➳ [A∂íƭყα](https://t.me/uttarpradeshi_17)  ☆》....》☆☆╮
 """
 
 __mod_name__ = "⚡Nᴏᴛᴇs⚡"
