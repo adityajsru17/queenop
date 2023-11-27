862c130807c469fd8909c9537f81dcd91757fcde
adityajsru17 <148866502+adityajsru17@users.noreply.github.com>
2023-11-27 16:31:30 +0530

    Update disable.py

--- MukeshRobot/modules/disable.py
+++ MukeshRobot/modules/disable.py
@@ -349,7 +349,7 @@ if is_module_loaded(FILENAME):
     ❍ /disablemodule <ᴍᴏᴅᴜʟᴇ ɴᴀᴍᴇ>*:* ᴅɪsᴀʙʟᴇ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴀᴛ ᴍᴏᴅᴜʟᴇ
     ❍ /listcmds*:* ʟɪsᴛ ᴀʟʟ ᴘᴏssɪʙʟᴇ ᴛᴏɢɢʟᴇᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+╰☆☆ : ̗̀➛ Bყ ➳ [Aɱαɳ](https://t.me/hoi_itz_mee) •.¸♡ ☆☆╮
     """
 
     __mod_name__ = " ♨️Dɪsᴀʙʟᴇ♨️"
